14fc803366bd9f90ce1d228a8a801699a3b9fd1e
Abigail Rolling <arolling@gmail.com>
2016-02-12 13:58:44 -0800

    Following along with epicodus videos

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,48 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+  // Unit testing
+
+  //Integration testing
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Square finder");
+  }
+
+  @Test
+  public void squareTestisASquare(){
+    goTo("http://localhost:4567/");
+    fill("#length").with("5");
+    fill("#width").with("5");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your rectangle is a square!");
+  }
+  @Test
+  public void squareTestisNotASquare(){
+    goTo("http://localhost:4567/");
+    fill("#length").with("5");
+    fill("#width").with("9");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your rectangle isn't a square!");
+  }
+}
